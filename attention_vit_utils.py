9e7cdeb59bbd26eba55ec1d2e9cbf727c251d4bd
Christopher Bennewitz <cbennewitz@student.ethz.ch>
2025-02-26 16:04:40 +0100

    my neural network scripts for H2O

---
+++ attention_vit_utils.py
@@ -0,0 +1,171 @@
+import random
+import torch
+import torch.nn as nn
+import numpy as np
+import glob
+from natsort import natsorted
+from transformers import ViTModel
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def soft_iou(pred, gt, eps=1e-6):
+    
+    intersection = torch.min(pred, gt).sum(dim=(1,2,3))
+    union = torch.max(pred, gt).sum(dim=(1,2,3))
+    siou = torch.mean((intersection + eps) / (union + eps))
+    return siou.item()
+
+def top_3(logits, gt):
+    _, top_5_pred = torch.topk(logits, 3, dim=1)
+    gt = gt.view(-1, 1)
+    correct = torch.sum(top_5_pred.eq(gt).sum(dim=1)).item()
+    return correct
+
+
+class TrainData(torch.utils.data.DataLoader):
+    def __init__(self, data_root):
+        self.img_paths = data_root + "sequences_train/*npy"
+        self.img_paths = natsorted(glob.glob(self.img_paths))
+        self.hand_paths = data_root + "hand_masks_train_resized/*npy"
+        self.hand_paths = natsorted(glob.glob(self.hand_paths))
+        self.obj_paths = data_root + "obj_masks_train_resized/*npy"
+        self.obj_paths = natsorted(glob.glob(self.obj_paths))
+        
+        self.labels = np.load(data_root + "action_train.npy")
+
+    def __len__(self):
+        return len(self.img_paths)
+    
+    def __getitem__(self, idx):
+        img = np.load(self.img_paths[idx]).astype(np.float32)
+        img_square = torch.from_numpy(img).float()
+        hand_mask = np.load(self.hand_paths[idx]).astype(np.float32) / 255.0
+        hand_mask = torch.from_numpy(hand_mask).float()
+        obj_mask = np.load(self.obj_paths[idx]).astype(np.float32) / 255.0
+        obj_mask = torch.from_numpy(obj_mask).float()
+        label = int(self.labels[idx])
+        return img_square, hand_mask, obj_mask, label
+
+class ValData(torch.utils.data.DataLoader):
+    def __init__(self, data_root):
+        self.img_paths = data_root + "sequences_val/*npy"
+        self.img_paths = natsorted(glob.glob(self.img_paths))
+        self.hand_paths = data_root + "hand_masks_val_resized/*npy"
+        self.hand_paths = natsorted(glob.glob(self.hand_paths))
+        self.obj_paths = data_root + "obj_masks_val_resized/*npy"
+        self.obj_paths = natsorted(glob.glob(self.obj_paths))
+
+        self.labels = np.load(data_root + "action_val.npy")
+    
+    def __len__(self):
+        return len(self.img_paths)
+    
+    def __getitem__(self, idx):
+        img = np.load(self.img_paths[idx]).astype(np.float32)
+        img_square = torch.from_numpy(img).float()
+        hand_mask = np.load(self.hand_paths[idx]).astype(np.float32) / 255.0
+        hand_mask = torch.from_numpy(hand_mask).float()
+        obj_mask = np.load(self.obj_paths[idx]).astype(np.float32) / 255.0
+        obj_mask = torch.from_numpy(obj_mask).float() 
+        label = int(self.labels[idx])
+        return img_square, hand_mask, obj_mask, label
+    
+class TestData(torch.utils.data.DataLoader):
+    def __init__(self, data_root):
+        self.img_paths = data_root + "sequences_test/*npy"
+        self.img_paths = natsorted(glob.glob(self.img_paths))
+    
+    def __len__(self):
+        return len(self.img_paths)
+    
+    def __getitem__(self, idx):
+        img = np.load(self.img_paths[idx]).astype(np.float32)
+        img_square = torch.from_numpy(img).float()
+        return img_square
+
+def preprocess(preprocessor, sequences):
+    processed_sequences = []
+    for i in range(sequences.shape[0]):
+        sequence = sequences[i]
+        processed_sequence = preprocessor(images = sequence, return_tensors='pt', size=496)
+        processed_sequences.append(processed_sequence['pixel_values'])
+    processed_sequences = torch.stack(processed_sequences)
+    return processed_sequences
+
+class ViTMLPModel(nn.Module):
+    def __init__(self):
+        super(ViTMLPModel, self).__init__()
+        self.vit = ViTModel.from_pretrained("google/vit-base-patch16-224")
+        self.vit.config.image_size = 496
+        vit_feature_dim = self.vit.config.hidden_size
+        self.mlp_input_dim = vit_feature_dim * 8
+        
+        self.mlp = nn.Sequential(
+            nn.Linear(self.mlp_input_dim, 3000),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(3000, 1000),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(1000, 300),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(300, 37)
+        )
+    
+    def forward(self, sequences):
+        
+        features = []
+        hand_attentions = []
+        obj_attentions = []
+
+        for i in range(sequences.shape[0]):
+            sequence = sequences[i]
+
+            outputs = self.vit(sequence, output_attentions=True, interpolate_pos_encoding=True)
+            image_features = outputs['pooler_output']
+            attentions_out = outputs['attentions']
+
+            last_layer_attentions = attentions_out[-1]
+
+            cls_attention_all_heads = last_layer_attentions.mean(dim=1)
+            cls_to_patches_attention = cls_attention_all_heads[:, 0, 1:]
+
+            grid_size = int(cls_to_patches_attention.shape[1] ** 0.5)
+            cls_attention_2d_batch = cls_to_patches_attention.reshape(-1, grid_size, grid_size)
+
+            num_heads = last_layer_attentions.shape[1]
+            hand_attention = last_layer_attentions[:, 0:6, 0, 1:]
+            obj_attention = last_layer_attentions[:, 6:12, 0, 1:]
+
+            hand_attention = hand_attention.reshape(-1, 6, grid_size, grid_size)
+            obj_attention = obj_attention.reshape(-1, 6, grid_size, grid_size)
+
+            hand_attention = (hand_attention - hand_attention.min()) / (hand_attention.max() - hand_attention.min())
+            obj_attention = (obj_attention - obj_attention.min()) / (obj_attention.max() - obj_attention.min())
+
+            mean_hand = torch.mean(hand_attention, dim=1)
+            mean_obj = torch.mean(obj_attention, dim=1)
+
+            features.append(image_features)
+            hand_attentions.append(mean_hand)
+            obj_attentions.append(mean_obj)
+
+        features = torch.stack(features)
+        concatenated_features = features.view(features.shape[0], 8*768)
+        hand_attentions = torch.stack(hand_attentions)
+        obj_attentions = torch.stack(obj_attentions)
+
+        output = self.mlp(concatenated_features)
+
+        return output, hand_attentions, obj_attentions
+        
+
---
+++ no_attention_vit.py
@@ -0,0 +1,268 @@
+from transformers import ViTImageProcessor, ViTModel
+from PIL import Image
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+import wandb
+import os
+from tqdm import tqdm
+import random
+import attention_vit_utils
+import json
+from datetime import datetime
+
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+seed = 7
+attention_vit_utils.set_seed(seed)
+batch_size = 1
+wandb.init(project="no_attention_transformer")
+dataset_root = "/cluster/scratch/cbennewitz/"
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+images_heatmaps_path = f"run_no_attention_{timestamp}"
+os.mkdir(images_heatmaps_path)
+
+def log_images_and_heatmaps(images, hand_attentions, obj_attentions, mode, epoch):
+    for i in range(images.shape[1]):
+        image = images[0, i, :, :, :].numpy().astype(np.uint8)
+        hand_attention = hand_attentions[0, i, :, :].detach().cpu().numpy()
+        obj_attention = obj_attentions[0, i, :, :].detach().cpu().numpy()
+        plt.figure(figsize=(5, 5))
+        plt.imshow(image)
+        plt.axis("off")
+        np.save(f"/cluster/home/cbennewitz/{images_heatmaps_path}/{mode}_{epoch+1}_{i}Input_Image.npy", image)
+        image_plt = wandb.Image(plt, caption=f"{mode}_{epoch+1} Input Image {i+1}")
+        wandb.log({f"{mode}_input_image_{i+1}": image_plt})
+        plt.close()
+
+        plt.figure(figsize=(5,5))
+        plt.imshow(hand_attention, cmap="jet")
+        plt.axis("off")
+        np.save(f"/cluster/home/cbennewitz/{images_heatmaps_path}/{mode}_{epoch+1}_{i}Hand_Attention_Map.npy", hand_attention)
+        hand_attention_plt = wandb.Image(plt, caption=f"{mode}_{epoch+1} Hand Attention Map {i+1}")
+        wandb.log({f"{mode}_Hand_Attention_MAP_{i+1}": hand_attention_plt})
+        plt.close()
+
+        plt.figure(figsize=(5,5))
+        plt.imshow(obj_attention, cmap="jet")
+        plt.axis("off")
+        np.save(f"/cluster/home/cbennewitz/{images_heatmaps_path}/{mode}_{epoch+1}_{i}Obj_Attention_Map.npy", obj_attention)
+        obj_attention_plt = wandb.Image(plt, caption=f"{mode}_{epoch+1} Obj Attention Map {i+1}")
+        wandb.log({f"{mode}_Obj_Attention_MAP_{i+1}": obj_attention_plt})
+        plt.close()
+
+
+def main():
+    preprocessor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224")
+
+    model = attention_vit_utils.ViTMLPModel()
+    model.to(device)
+
+    train_data = attention_vit_utils.TrainData(dataset_root)
+    val_data = attention_vit_utils.ValData(dataset_root)
+    test_data = attention_vit_utils.TestData(dataset_root)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+    num_epochs = 30
+    current_best = np.inf
+    
+    # Loss weights
+    alpha = 1.0
+    beta = 100.0
+    gamma = 100.0
+    
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss: float = 0.0
+        running_class_loss: float = 0.0
+        running_hand_loss: float = 0.0
+        running_obj_loss: float = 0.0
+        running_hand_iou: float = 0.0
+        running_obj_iou: float = 0.0
+        counter_train: int = 0
+        train_correct: int = 0
+        train_top3: int = 0
+        train_total: int = 0
+        for sequences, hand_masks, obj_masks, labels in tqdm(train_loader):
+            optimizer.zero_grad()
+
+            processed_sequences = attention_vit_utils.preprocess(preprocessor, sequences)
+            processed_sequences = processed_sequences.to(device)
+            labels = labels.to(device)
+            hand_masks = hand_masks.to(device)
+            obj_masks = obj_masks.to(device)
+
+            output, hand_attentions, obj_attentions = model(processed_sequences)
+
+            hand_attentions = F.interpolate(hand_attentions, size=(hand_masks.shape[2], hand_masks.shape[3]), mode="nearest")
+            obj_attentions = F.interpolate(obj_attentions, size=(obj_masks.shape[2], obj_masks.shape[3]), mode="nearest")
+
+            class_loss = criterion(output, labels)
+
+            hand_loss = F.mse_loss(hand_attentions, hand_masks)
+            obj_loss = F.mse_loss(obj_attentions, obj_masks)
+
+            loss = alpha * class_loss + beta * hand_loss + gamma * obj_loss
+
+            running_hand_iou += attention_vit_utils.soft_iou(hand_attentions, hand_masks)
+            running_obj_iou += attention_vit_utils.soft_iou(obj_attentions, obj_masks)
+            running_loss += loss.item()
+            running_class_loss += class_loss.item()
+            running_hand_loss += hand_loss.item()
+            running_obj_loss += obj_loss.item()
+
+            class_loss.backward()
+            optimizer.step()
+
+            _, predicted = torch.max(output, 1)
+            train_correct += (predicted == labels).sum().item()
+            train_top3 += attention_vit_utils.top_3(output, labels)
+
+            train_total += labels.size(0)
+            
+
+
+            if counter_train == 100:
+                log_images_and_heatmaps(sequences, hand_attentions, obj_attentions, mode="train",epoch=epoch)
+
+            counter_train += 1
+
+        avg_train_loss = running_loss / counter_train
+        avg_class_loss = running_class_loss / counter_train
+        avg_hand_loss = running_hand_loss / counter_train
+        avg_obj_loss = running_obj_loss / counter_train
+        avg_hand_iou = running_hand_iou / counter_train
+        avg_obj_iou = running_obj_iou / counter_train
+        train_accuracy = train_correct / train_total
+        train_top_3_accuracy = train_top3 / train_total
+        
+
+        wandb.log({"train_loss": avg_train_loss,
+                   "class_loss": avg_class_loss,
+                   "hand_loss": avg_hand_loss,
+                   "objects_loss": avg_obj_loss,
+                   "hand_iou": avg_hand_iou,
+                   "obj_iou": avg_obj_iou,
+                   "train_accuracy": train_accuracy,
+                   "train_top3_accuracy": train_top_3_accuracy,
+                   "epoch": epoch + 1})
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Class Loss: {avg_class_loss}, Hand Loss: {avg_hand_loss}, Obj Loss: {avg_obj_loss}, Hand IoU: {avg_hand_iou}, Obj IoU: {avg_obj_iou}, Train Accuracy: {train_accuracy*100:.2f}%, Train Top 3 Accuracy: {train_top_3_accuracy*100:.2f}%")
+
+        model.eval()
+        val_running_loss: float = 0.0
+        val_class_loss: float = 0.0
+        val_hand_loss: float = 0.0
+        val_obj_loss: float = 0.0
+        val_hand_iou: float = 0.0
+        val_obj_iou: float = 0.0
+        counter_val: int = 0
+        val_correct: int = 0
+        val_top3: int = 0
+        val_total: int = 0
+        with torch.no_grad():
+            for sequences, hand_masks, obj_masks, labels in tqdm(val_loader):
+                processed_sequences = attention_vit_utils.preprocess(preprocessor, sequences)
+                processed_sequences = processed_sequences.to(device)
+                
+                labels = labels.to(device)
+                hand_masks = hand_masks.to(device)
+                obj_masks = obj_masks.to(device)
+
+                output, hand_attentions, obj_attentions = model(processed_sequences)
+
+                hand_attentions = F.interpolate(hand_attentions, size=(hand_masks.shape[2], hand_masks.shape[3]), mode="nearest")
+                obj_attentions = F.interpolate(obj_attentions, size=(obj_masks.shape[2], obj_masks.shape[3]), mode="nearest")
+
+
+                class_loss = criterion(output, labels)
+                hand_loss = F.mse_loss(hand_attentions, hand_masks)
+                obj_loss = F.mse_loss(obj_attentions, obj_masks)
+
+                loss = alpha * class_loss + beta * hand_loss + gamma * obj_loss
+
+                val_hand_iou += attention_vit_utils.soft_iou(hand_attentions, hand_masks)
+                val_obj_iou += attention_vit_utils.soft_iou(obj_attentions, obj_masks)
+
+                val_running_loss += loss.item()
+                val_class_loss += class_loss.item()
+                val_hand_loss += hand_loss.item()
+                val_obj_loss += obj_loss.item()
+
+                _, predicted = torch.max(output, 1)
+                val_correct += (predicted == labels).sum().item()
+                val_top3 += attention_vit_utils.top_3(output, labels)
+                val_total += labels.size(0)
+
+                if counter_val == 25:
+                    log_images_and_heatmaps(sequences, hand_attentions, obj_attentions, mode="val", epoch=epoch)
+                
+                counter_val += 1
+
+        avg_val_loss = val_running_loss / counter_val
+        avg_val_class_loss = val_class_loss / counter_val
+        avg_val_hand_loss = val_hand_loss / counter_val
+        avg_val_obj_loss = val_obj_loss / counter_val
+        avg_val_hand_iou = val_hand_iou / counter_val
+        avg_val_obj_iou = val_obj_iou / counter_val
+        val_accuracy = val_correct / val_total
+        val_top_3_accuracy = val_top3 / val_total
+
+        wandb.log({"val_loss": avg_val_loss,
+                   "val_class_loss": avg_val_class_loss,
+                   "val_hand_loss": avg_val_hand_loss,
+                   "val_obj_loss": avg_val_obj_loss,
+                   "val_hand_iou": avg_val_hand_iou,
+                   "val_obj_iou": avg_val_obj_iou,
+                   "val_accuracy": val_accuracy,
+                   "val_top3_accuracy": val_top_3_accuracy,
+                   "epoch": epoch + 1})
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Val Loss: {avg_val_loss:.4f}, Val Class Loss: {avg_val_class_loss}, Val Hand Loss: {avg_val_hand_loss}, Val Obj Loss: {avg_val_obj_loss}, Val Hand IoU: {avg_val_hand_iou}, Val Obj IoU: {avg_val_obj_iou}, Val Accuracy: {val_accuracy*100:.2f}%, Val Top 3 Accuracy: {val_top_3_accuracy*100:.2f}%")
+
+        if current_best > avg_val_loss:
+            current_best = avg_val_loss
+            counter_test = 0
+            predictions = []
+            model.eval()
+            with torch.no_grad():
+                for sequences in tqdm(test_loader):
+                    processed_sequences = attention_vit_utils.preprocess(preprocessor,sequences)
+                    processed_sequences = processed_sequences.to(device)
+                    output, hand_attentions, obj_attentions = model(processed_sequences)
+
+                    _, predicted = torch.max(output, 1)
+                    predictions.extend(predicted.cpu().tolist())
+                    
+                    if counter_test == 1 or counter_test == 100:
+                        log_images_and_heatmaps(sequences, hand_attentions, obj_attentions, mode="test", epoch=epoch)
+                    
+                    counter_test += 1
+
+            json_data = {str(i + 1): predictions[i] for i in range(len(predictions))}
+            with open("no_attention_test_predictions_best.json", "w") as f:
+                json.dump({"predictions": json_data}, f)
+            print("Test predictions saved to no_attention_test_predictions_best.json")
+
+            model_save_path = "no_attention_transformer_model_best.pth"
+            torch.save(model.state_dict(), model_save_path)
+            print(f"Model saved to {model_save_path}")
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    main()
